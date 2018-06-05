393354a7397d9be764183a58324f0adeea06a8d8
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2018-06-05 16:24:57 +0430

    change phpmyadmin config add header for bypass nginx cache

--- app/app/public/index.php
+++ app/app/public/index.php
@@ -1,1 +1,9 @@
-<?php phpinfo();
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+header("Cache-Control: ", false);
+header("Expires: ", gmdate('r', 0));
+header("X-Accel-Expires: ", gmdate('r', 0));
+header("Last-Modified: ", gmdate('r'));
+header("Pragma: no-cache");
+phpinfo();
+
